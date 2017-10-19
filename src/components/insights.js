c251d5a0c4ec65fcbf15ca9468d96b9ac1ab2ca7
Santhosh Maila <santhosh.maila@wdc.com>
2017-10-18 21:23:26 -0700

    removing the japanese content from the twitter

--- src/components/insights.js
+++ src/components/insights.js
@@ -5,8 +5,7 @@ export default class Insights extends Component {
   render() {
     return (
       <div className="container" >
-        <TwitterTimeline user="bookbild_inc" />
-        <TwitterTimeline widgetId="695868534455275520" chrome="noborders noheader" />
+        <TwitterTimeline user="bookbild_inc"/>
       </div>
     );
   }
